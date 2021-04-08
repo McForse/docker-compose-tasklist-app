1398dd74396cff994610a27fd3e9f0aa1e2887bd
Danil Vorozheykin <danilvda@gmail.com>
2021-04-08 11:27:21 +0300

    Fix: docker build

--- backend/src/main/java/com/mcforse/taskList/TaskListApplication.java
+++ backend/src/main/java/com/mcforse/taskList/TaskListApplication.java
@@ -1,12 +1,19 @@
 package com.mcforse.taskList;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class TaskListApplication {
 
 	public static void main(String[] args) {
+		String port = System.getenv("SERVER_PORT");
+		if (port == null) {
+			log.error("Environment variables not configured");
+			return;
+		}
 		SpringApplication.run(TaskListApplication.class, args);
 	}
 
